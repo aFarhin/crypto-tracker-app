ccf58e0c524dd9c664775170e29fba0b7cc7f1ae
aFarhin <ahmeda.farhin786@gmail.com>
2023-06-18 14:06:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {  BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home'
+import DashboardPage from './pages/Dashboard'
+import CoinPage from './pages/Coin'
+import ComparePage from './pages/compare'
+import WatchlistPage from './pages/watchlist'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Dashboard" element={<DashboardPage />} />
+          <Route path="/Coin/:id" element={<CoinPage />} />
+           <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
